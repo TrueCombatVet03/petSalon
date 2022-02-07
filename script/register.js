0884a1b33b483c2e8e4acb1f77bd19fd1cadb6bc
TrueCombatVet03 <97132973+TrueCombatVet03@users.noreply.github.com>
2022-02-06 18:07:20 -0600

    Register section/form add
    Here I added the register section and a form with values.
    register button works and adds new line to form.
    added css/js

--- script/register.js
+++ script/register.js
@@ -9,49 +9,9 @@ let salon={
         open:"9:00 am",
         close:"5:00 pm"
     },
-    pet:[
-        {
-            name:"Scooby",
-            age:60,
-            gender:"male",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"Shaggy",
-            phone:"555-555-5555"
-        },
-        {
-            name:"Betsy",
-            age:10,
-            gender:"female",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"John",
-            phone:"555-777-5555"
-        },
-        {
-            name:"Sarge",
-            age:5,
-            gender:"male",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"Kevin",
-            phone:"555-585-5555"
-        },
-        {
-            name:"Army",
-            age:2,
-            gender:"male",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"Mike",
-            phone:"555-989-5555"
-        },
-        
-    ]
+    pets:[]
+
 }
-console.log(salon.pet);
-
-
 
 function displayPetAges(){
     for(let i=0;i<salon.pet.length;i++){
@@ -59,6 +19,52 @@ function displayPetAges(){
     }
 }
 
-//alert('Pets Registered: 4')
+//create the constructor
+function Pet(name,age,gender){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+}
+//create three pets using the constructor
+let sarge = new Pet("Sarge",12,"male");
+salon.pets.push(sarge);
 
-displayPetAges();
+
+let petName = document.getElementById("txtName");
+let petAge = document.getElementById("txtAge");
+let petGender = document.getElementById("txtGender");
+
+//register section//
+function register(){
+    console.log("Register");
+    let thePet = new Pet(petName.value,petAge.value,petGender.value);
+    console.log(thePet);
+    salon.pets.push(thePet);
+
+    
+    console.log(salon.pets);
+}
+//register to form section//
+let btnAdd = document.querySelector('button');
+let table = document.querySelector('table');
+
+let nameInput= document.querySelector('#txtName');
+let ageInput= document.querySelector('#txtAge');
+let genderInput= document.querySelector('#txtGender');
+
+btnAdd.addEventListener('click', () =>{
+    let name = nameInput.value;
+    let age = ageInput.value;
+    let gender = genderInput.value;
+
+    let template = `
+    <tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${gender}</td>      
+    </tr>`;
+
+    table.innerHTML += template;
+});
+
+
